2be1b17c491e82376ac45e8c7df990a147e9554f
Juan Carlos Benjamín Somohano Murrieta <juan.somur@gmail.com>
2019-03-28 22:31:26 -0600

    Campos editables

--- src/app/productos/productos.component.ts
+++ src/app/productos/productos.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { ProductoService } from '../servicios/productos.service';
 
 import { NotifierService } from 'angular-notifier';
@@ -11,10 +11,11 @@ import { NotifierService } from 'angular-notifier';
 export class ProductosComponent implements OnInit {
 
   private notifier: NotifierService;
-
+  public camposHabilitados: boolean;
   constructor(public productoService: ProductoService, notifier: NotifierService) {
     this.notifier = notifier;
-   }
+    this.camposHabilitados = false;
+  }
 
   productos: Array<any> = []
 
@@ -32,11 +33,16 @@ export class ProductosComponent implements OnInit {
     })
   }
 
-  public showNotification(event): void{
-    this.notifier.notify( 'info', 'Mensaje de información complementaria' );
-    this.notifier.notify( 'success', 'Mensaje de éxito, sí se pudo' );
-    this.notifier.notify( 'warning', 'Mensaje de advertencia algo raro pasó' );
-    this.notifier.notify( 'error', 'Mensaje de error, algo salió mal' );
+  public showNotification(event): void {
+    this.notifier.notify('info', 'Mensaje de información complementaria');
+    this.notifier.notify('success', 'Mensaje de éxito, sí se pudo');
+    this.notifier.notify('warning', 'Mensaje de advertencia algo raro pasó');
+    this.notifier.notify('error', 'Mensaje de error, algo salió mal');
+  }
+
+  public habilitarCampos() {
+    console.log("Habilitando campos...");
+    this.camposHabilitados = !this.camposHabilitados;
   }
 
 }
