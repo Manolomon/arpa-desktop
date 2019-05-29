8839d3c2121122682bc4f72cb74b8e5207b60de5
Daniel Escamilla <danix100@hotmail.com>
2019-05-29 09:34:56 -0500

    Servicio para guardar datos personales

--- src/app/models/MiembroInterface.ts
+++ src/app/models/MiembroInterface.ts
@@ -2,5 +2,10 @@ export interface Miembro {
     id?: string;
     nombre?: string;
     correo?: string;
+    passGenerada?: string;
     rol?: string;
+    facultad?: string;
+    institucion?: string;
+    puesto?: string;
+    sni?: string;
 }
